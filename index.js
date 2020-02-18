6270101b24dce1e82b1cbeae7a7931338a6f8149
Nathan Brickett <natey37@gmail.com>
2020-02-18 10:25:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file
+function findMatching(list, name){
+    // console.log(name)
+    // console.log(list.filter(function(driverName){
+    // driverName.toLowerCase() === name.toLowerCase();}))
+    return list.filter(function(driverName){
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+
+    // return list.filter(function(driverName){
+    //     console.log(list.filter(function(driverName))
+    //     driverName.toLowerCase() === name.toLowerCase();
+    // })
+}
+
+function fuzzyMatch(list, letters){
+    let lengthOfName = letters.length
+    return list.filter(function(driverName){
+        return driverName.slice(0, lengthOfName) === letters
+    });
+}
+
+function matchName(list, name){
+    return list.filter(function(driver){
+        return driver.name.toLowerCase() === name.toLowerCase()
+    })
+}
